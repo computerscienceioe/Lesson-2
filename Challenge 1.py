6fd0f4d5b6d44aa8d54c474ecb3749389205d33b
MrBenjaminCounter <77814190+MrBenjaminCounter@users.noreply.github.com>
2021-01-24 20:34:42 +0000

    Create Challenge 1.py

---
+++ Challenge 1.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.speed(5)
+
+for i in range(3):
+  turtle.forward(100)
+  turtle.left(120)
+
+for i in range(5):
+  turtle.forward(100)
+  turtle.left(360.0/5)
+
+for i in range(8):
+  turtle.forward(100)
+  turtle.left(360.0/8)
